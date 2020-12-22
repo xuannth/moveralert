5ec74da890d909ca259a00f15395d630e0215347
an pham <an.anptv@gmail.com>
2020-12-22 11:47:57 +0700

    Clean code

--- src/test/java/asia/twentyci/steps/serenity/LoginSteps.java
+++ src/test/java/asia/twentyci/steps/serenity/LoginSteps.java
@@ -1,19 +1,20 @@
 package asia.twentyci.steps.serenity;
 
-import asia.twentyci.pages.LoginPage;
-import net.thucydides.core.annotations.Step;
+import static asia.twentyci.pages.LoginPage.SUBMIT_BUTTON;
 import static asia.twentyci.pages.LoginPage.USER_EMAIL;
 import static asia.twentyci.pages.LoginPage.USER_PASSWORD;
-import static asia.twentyci.pages.LoginPage.SUBMIT_BUTTON;
+
+import asia.twentyci.pages.LoginPage;
+import net.thucydides.core.annotations.Step;
 
 public class LoginSteps {
 
-	// LoginPage onLoginpage;
+	LoginPage onLoginpage;
 
 	@Step
 	public void loginSystemWith(String user_email, String user_password) {
-		$(USER_EMAIL).type(user_email);
-		$(USER_PASSWORD).type(user_password);
-		element(SUBMIT_BUTTON).click();
+		onLoginpage.find(USER_EMAIL).type(user_email);
+		onLoginpage.find(USER_PASSWORD).type(user_password);
+		onLoginpage.find(SUBMIT_BUTTON).click();
 	}
 }
